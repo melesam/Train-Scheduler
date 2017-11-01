e2c0b7c164aba8fb85b355d54f18b9b9dd93a454
Melesa Martin <melesamartin@melesamrtinsmbp.attlocal.net>
2017-11-01 09:38:07 -0500

    first push

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUcek6amZPxoxZZWU3-NcHCikYWJD6Lv8",
+    authDomain: "trainscheduler-dd0bc.firebaseapp.com",
+    databaseURL: "https://trainscheduler-dd0bc.firebaseio.com",
+    projectId: "trainscheduler-dd0bc",
+    storageBucket: "trainscheduler-dd0bc.appspot.com",
+    messagingSenderId: "1037127325137"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
